f21f5f54e29c9038c980b439cc8e07b325d1630e
Azam <abdurazakovazam01@gmail.com>
2022-11-24 16:27:23 +0500

    commit

--- lotteryapp/views.py
+++ lotteryapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    def gimma_chance(iterations, variants):
+        final_chance = 100
+        for i in range(iterations):
+            final_chance = final_chance / variants
+        return final_chance
+
+    c = gimma_chance(2, 4)
+
+    context = {
+        'chance': c,
+    }
+    return render(request, 'lotteryproject/index.html', context)
